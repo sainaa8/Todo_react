45cecab15ec97162085662e05a123102118a50c3
ganhuyagdamdinnym <gdamdinnym@gmail.com>
2024-01-06 13:19:24 +0800

    couldnt change status

--- todoreact/src/Components/Card.jsx
+++ todoreact/src/Components/Card.jsx
@@ -2,9 +2,10 @@ import { useState } from "react";
 
 const MyCard = (props) => {
   const { done } = props;
+
   return (
     <div>
-      <div className="w-[400px] h-fit bg-amber-600 rounded-xl">
+      <div className=" h-fit bg-amber-600 rounded-xl">
         <div className="w-[100%] h-[60px]text-[30px] font-sans  items-center 	">
           <div className="ml-[20px] text-[30px] flex gap-[10px]">
             <div className="font-bold">{done}</div>
@@ -14,20 +15,20 @@ const MyCard = (props) => {
         <div className="w-[100%] h-[70px] flex justify-center items-center">
           <div className=" w-[95%] h-[70%] flex text-[30px] gap-[20px] font-sans items-center  hover:bg-amber-800 rounded-lg">
             <div className="ml-[40px]">+</div>
-            <div className=" ">
-              <div>add card</div>
-            </div>
+            <div className="w-[200px] flex ">add card</div>
           </div>
         </div>
       </div>
     </div>
   );
 };
-const Selections = ({ option, getOptionValue }) => {
+const Selections = ({ option }) => {
+  const [statussvalue, setstatuss] = useState("");
   return (
     <select
+      onChange={(e) => setstatuss(e.target.value)}
+      value={statuss}
       className="border border-black rounded-lg w-[100%]"
-      onChange={getOptionValue}
     >
       {option.map((item, index) => (
         <option key={index} value={item}>
@@ -48,17 +49,43 @@ const headers = [
 ];
 export const Cards = () => {
   const [card, setCard] = useState(false);
+  const [array, setArray] = useState([]);
+  const [donevalue, setDonevalue] = useState("");
+  const [descriptionvalue, setDesctionvalue] = useState("");
+  // const [donevalue, setDonevalue] = useState("");
+  // const [donevalue, setDonevalue] = useState("");
+
+  // []
+
   const getModal = () => {
     setCard((item) => !item);
   };
+  const addtask = () => {
+    const newtitle = [...array];
+    newtitle.push({
+      title: donevalue,
+      description: descriptionvalue,
+      status: statussvalue,
+    });
+    console.log("ee", newtitle);
+    const temp = JSON.stringify(newtitle);
+    localStorage.setItem("array", temp);
+    setArray(newtitle);
+    setDonevalue("");
+    setDesctionvalue("");
+    // localStorage.setItem("array2", "jjjjj");
+    // setTimeout(() => {
+    // }, 5000);
+  };
+  //console.log(donevalue);
   return (
     <div>
       <div>
         <div className="flex gap-12">
-          {headers.map((el, i) => {
+          {headers.map((item, i) => {
             return (
               <div onClick={getModal} key={i}>
-                <MyCard done={el.el} />
+                <MyCard done={item.el} />
               </div>
             );
           })}
@@ -67,16 +94,24 @@ export const Cards = () => {
       <div>
         {card && (
           <div>
-            <div className="fixed left-[41%] top-[300px] bg-[#fff] w-[500px] flex flex-col font-sans gap-8 p-8 rounded-lg">
+            <div className="fixed left-[41%] top-[250px] bg-[#fff] w-[500px] flex flex-col font-sans gap-8 p-8 rounded-lg">
               <h1 className="text-5xl font-bold ">Add Task</h1>
               <div className="flex gap-4 text-[25px] flex-col ">
                 <div>
                   <h2>Title</h2>
-                  <input className="border border-black rounded-lg pl-[10px] w-[100%]" />
+                  <input
+                    onChange={(e) => setDonevalue(e.target.value)}
+                    value={donevalue}
+                    className="border border-black rounded-lg pl-[10px] w-[100%]"
+                  />
                 </div>
                 <div>
                   <h2>Description</h2>
-                  <input className="border border-black rounded-lg w-[100%] pl-[10px]" />
+                  <input
+                    onChange={(e) => setDesctionvalue(e.target.value)}
+                    value={descriptionvalue}
+                    className="border border-black rounded-lg w-[100%] pl-[10px]"
+                  />
                 </div>
                 <div className="">
                   <h2>Status</h2>
@@ -87,11 +122,13 @@ export const Cards = () => {
                   <Selections option={states} />
                 </div>
               </div>
-              <div
-                onClick={getModal}
-                className="bg-black py-[10px] text-white text-[30px] flex  justify-center rounded-lg"
-              >
-                add task
+              <div onClick={() => addtask()}>
+                <div
+                  onClick={getModal}
+                  className="bg-black py-[10px] text-white text-[30px] flex  justify-center rounded-lg"
+                >
+                  add task
+                </div>
               </div>
             </div>
           </div>
